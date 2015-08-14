20ef88353ba3c80187e0d52eadfd4ae45131343d
Drew Price <drewprice@users.noreply.github.com>
2015-08-14 13:58:36 -0400

    Add solution

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 154, 'm',  'brown', 'grumpy', 1),
+('Rowdy', 12, 'm', 'blue', 'rowdy', 0),
+('Tabitha', 7, 'f', 'brown', 'nice', 1),
+('Sergeant Brown', 3, 'm', 'red', 'grumpy', 1),
+('Melissa', 8, 'f', 'blue', 'nice', 1),
+('Grinch', 15, 'm', 'green', 'grumpy', 0),
+('Wendy', 11, 'm', 'brown', 'flustered', 1),
+(null, 13, 'm', 'brown', 'confused', 0);
